dfcd137cdc7123fa735e4b63b0f333a220850c50
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-17 14:07:08 +0300

    Tasks12 solved

--- Lect_C2_17122022/Task1/Program.cs
+++ Lect_C2_17122022/Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+int Max(int a, int b, int c)
+{
+    int rez = a;
+    if (b > a) {rez = b;}
+    if (c > rez) {rez = c;}
+    return rez;
+}
+int a1 = 7;
+int a2 = 29;
+int a3 = 3;
+int a4= 12;
+int a5 = 11;
+int a6 = 47;
+int a7 = 23;
+int a8 = 12;
+int a9 = 15;
+
+
+int max = Max(Max(a1 ,a2, a3), Max(a4 ,a5, a6), Max(a7 ,a8, a9));
+Write(max);
---
+++ Lect_C2_17122022/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+
+int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+int length = collection.Length;
+int index = 0;
+while (index < length)
+{
+ collection[index] = new Random().Next(1, 10);
+ index++;
+}
+}
+
+void PrintArray(int[] col)
+{
+int count = col.Length;
+int position = 0;
+while (position < count)
+{
+Console.WriteLine(col[position]);
+position++;
+}
+}
+
+int IndexOf(int[] collection, int find)
+{
+int index = 0;
+int count = collection.Length;
+int position = -100;
+while (index < count)
+{
+ if(collection[index] == find)
+ {
+ position = index;
+ break;
+ }
+index++;
+}
+return position;
+}
+
+
+FillArray(array);
+PrintArray(array);
+
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
